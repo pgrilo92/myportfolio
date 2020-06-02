7693fb211b865e862daf1e4c03fd6972e8290ff5
Joaquim <pgrilo92@hotmail.com>
2020-06-02 08:16:14 -0400

    Updates

---
+++ precache-manifest.402ac0367bad6c93f4693a4a2a1a68bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf52655916f065fbe7a2a0f5906acfd3",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "38c0ccf0f786676e95a8",
+    "url": "/myportfolio/static/css/main.7dd37ccc.chunk.css"
+  },
+  {
+    "revision": "4d7cf9a7601cf6557c98",
+    "url": "/myportfolio/static/js/2.2bc1bc32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.2bc1bc32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c0ccf0f786676e95a8",
+    "url": "/myportfolio/static/js/main.b50ebc8c.chunk.js"
+  },
+  {
+    "revision": "b7233b44fe85679857af",
+    "url": "/myportfolio/static/js/runtime-main.86b85267.js"
+  },
+  {
+    "revision": "dd10dfb1f30dc7ff5d48c25ef58938a7",
+    "url": "/myportfolio/static/media/IMG_0060.dd10dfb1.jpg"
+  },
+  {
+    "revision": "8c89f2f063a95cec73548d8ca8433d45",
+    "url": "/myportfolio/static/media/alpha-blog.8c89f2f0.png"
+  },
+  {
+    "revision": "77d1c35438963b499efef6ec402ff860",
+    "url": "/myportfolio/static/media/business-code-coding-computer-270360.77d1c354.jpg"
+  },
+  {
+    "revision": "fccac5402f655f4ad707dae2be142efd",
+    "url": "/myportfolio/static/media/gogym-img.fccac540.png"
+  },
+  {
+    "revision": "48506486d42845935199de6552b62b6d",
+    "url": "/myportfolio/static/media/joaquim-web.48506486.png"
+  },
+  {
+    "revision": "7dcf6a8ce4872c6364b7e631345893ec",
+    "url": "/myportfolio/static/media/pc-office.7dcf6a8c.jpg"
+  },
+  {
+    "revision": "24dbefa1780faaf431d81356753f8570",
+    "url": "/myportfolio/static/media/photo-app.24dbefa1.png"
+  }
+]);
